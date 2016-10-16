df8ef4c11e2227be835ca959722adafdc557c49f
shavit <shavit@users.noreply.github.com>
2016-10-16 01:43:44 -0400

    🎉

--- lib/video_chat/router.ex
+++ lib/video_chat/router.ex
@@ -9,6 +9,20 @@ defmodule VideoChat.Router do
     |> send_resp(200, "Hello")
   end
 
+  # Accept video stream
+  post "/videos/stream" do
+    # stream to the clients
+    conn
+    |> send_repo(200, "Wait")
+  end
+
+  # Stream video
+  get "/videos/live" do
+    conn
+    |> put_resp_content_type("application/octet-stream")
+    |> send_resp(200, "ok")
+  end
+
   match _ do
     conn
     |> send_resp(404, "Not found")
