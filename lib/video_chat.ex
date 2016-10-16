53088ff40e4a274d829859fa6f2c0f086fe0d523
shavit <shavit@users.noreply.github.com>
2016-10-16 01:33:30 -0400

    🎉

--- lib/video_chat.ex
+++ lib/video_chat.ex
@@ -8,7 +8,7 @@ defmodule VideoChat do
 
     # Define workers and child supervisors to be supervised
     children = [
-      Plug.Adapters.Cowboy.child_spec(:http, VideoChat.Router, [], [port: 4001]),
+      # Plug.Adapters.Cowboy.child_spec(:http, VideoChat.Router, [], [port: 4001]),
       supervisor(VideoChat.Stack, [[:video_chat], [name: VideoChatServer]])
       # Starts a worker by calling: VideoChat.Worker.start_link(arg1, arg2, arg3)
       # worker(VideoChat.Worker, [arg1, arg2, arg3]),
--- lib/video_chat/server.ex
+++ lib/video_chat/server.ex
@@ -7,4 +7,15 @@ defmodule VideoChat.Server do
         [port: 4001])}
   end
 
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      Plug.Adapters.Cowboy.child_spec(:http, VideoChat.Router, [], [port: 4001]),
+    ]
+
+    opts = [strategy: :one_for_one, name: VideoChat.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
 end
