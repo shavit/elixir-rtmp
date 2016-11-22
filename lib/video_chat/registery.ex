04dfabf6a81f724d84c573f8af7e4961c68eae29
shavit <shavit@users.noreply.github.com>
2016-11-21 22:27:28 -0500

    🎉

--- lib/video_chat/registery.ex
+++ lib/video_chat/registery.ex
@@ -7,4 +7,12 @@ defmodule VideoChat.Registery do
   def start_link() do
     GenServer.start_link(__MODULE__, :ok, [])
   end
+
+  @doc """
+  Lookup the bucket pid for name stored in server
+  """
+  def lookup(server, name) do
+    GenServer.call(server, {:lookup, name})
+  end
+
 end
