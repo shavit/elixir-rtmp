e818344030205b83c461b85481b08d86719c9589
shavit <shavit@users.noreply.github.com>
2016-11-28 22:50:07 -0500

    🎉

--- test/receive_packets_test.exs
+++ test/receive_packets_test.exs
@@ -5,7 +5,7 @@ defmodule ReceivePacketsTest do
   test "should receive messages" do
     {:ok, socket} = :gen_udp.open(3010)
 
-    assert :ok == :gen_udp.send(socket, {127,0,0,1}, 3001, "Message 1")
+    assert :ok = :gen_udp.send(socket, {127,0,0,1}, 3001, "Message 1")
   end
 
 end
