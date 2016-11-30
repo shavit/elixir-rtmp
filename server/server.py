a96012721a2fd07c4a415765ec68d4e9b73c2b57
shavit <shavit@users.noreply.github.com>
2016-11-29 22:53:37 -0500

    🎉

--- server/server.py
+++ server/server.py
@@ -1,15 +1,28 @@
 import socket
+import signal
+import sys
+
+bucket = bytearray()
+
+def user_signals(signal, frame):
+    print('---> Terminating')
+    # with open('tmp/image_out_2.gif', 'wb') as f:
+    #     f.write(bucket)
+    with open('tmp/README.md', 'wb') as f:
+        f.write(bucket)
+    sys.exit(0)
 
 def run():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('127.0.0.1', 3001))
 
-    bucket = list()
-
     while True:
         buf = 2048
         data, _ = sock.recvfrom(buf)
-        print('---> Received ({}) {}'.format(type(data), data))
+        print('---> Received ({}) {}'.format(type(data), len(data)))
+        for d in data:
+            bucket.append(d)
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, user_signals)
     run()
