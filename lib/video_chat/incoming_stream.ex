b096a4a029f2644a31f10f2f9a0b413fe339421f
shavit <shavit@users.noreply.github.com>
2016-12-03 07:04:54 -0500

    🎉

--- lib/video_chat/incoming_stream.ex
+++ lib/video_chat/incoming_stream.ex
@@ -16,7 +16,9 @@ defmodule VideoChat.IncomingStream do
 
   # Incoming streaming data from the webcam.
   def handle_info({:udp, _socket, _ip, _port, data}, state) do
-    IO.inspect "---> Received #{byte_size(data)} bytes"
+    IO.inspect _socket
+    IO.inspect _ip
+    IO.inspect "---> Received #{byte_size(data)} bytes from #{_port}"
 
     # Write to the bucket
     VideoChat.EncodingBucket.add data
