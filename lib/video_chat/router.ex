32c1a77fba3b85e74555a97357d6f4a15c1f0811
shavit <shavit@users.noreply.github.com>
2017-02-03 02:42:43 -0500

    🎉

--- lib/video_chat/router.ex
+++ lib/video_chat/router.ex
@@ -86,6 +86,7 @@ defmodule VideoChat.Router do
   end
 
   # Live stream from the webcam or UDP connection.
+  # Need to parse the packets
   get "/videos/live/playlist" do
     IO.inspect "---> Getting live video stream"
 
