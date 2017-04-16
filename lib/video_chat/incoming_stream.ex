579adf84c07bb765f684ba486c2f65bf71bb549f
shavit <shavit@users.noreply.github.com>
2017-04-16 01:04:58 -0400

    🎉

--- lib/video_chat/incoming_stream.ex
+++ lib/video_chat/incoming_stream.ex
@@ -34,28 +34,27 @@ defmodule VideoChat.IncomingStream do
 
   defp write_data(message) do
     # File.write("tmp/picture-#{message.channel}.mp4", message.data)
-    File.write("tmp/video-#{message.channel}.mp4", message.data, [:append])
+    # File.write("tmp/picture-#{message.channel}.jpg", message.data, [:append])
+    File.write("tmp/picture-#{message.channel}.jpg", message.data)
+    # File.write("tmp/video-#{message.channel}.mp4", message.data, [:append])
   end
 
-  # Optional format
+  # Messages should not exceed 4000 bytes
   defp parse_message(message) do
     # channel: 001
     # resolution: 1 | 2 | 3 | 4
-    # size: 4000
     # data: binary
     <<
       channel :: little-unsigned-integer-size(32),
       resolution :: little-unsigned-integer-size(8),
-      size :: size(56),
       data :: binary
     >> = message
 
-    IO.inspect "C:#{channel} | R:#{resolution} | S:#{size}"
+    IO.inspect "C:#{channel} | R:#{resolution}"
 
     %{
       channel: channel,
       resolution: resolution,
-      size: size,
       data: data
     }
   end
