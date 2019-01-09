05a110b609e9be7746b91ae1dfe165bc4657c318
shavit <shavit@users.noreply.github.com>
2019-01-09 08:52:57 -0500

    Remove all dependencies

--- mix.exs
+++ mix.exs
@@ -6,29 +6,14 @@ defmodule VideoChat.Mixfile do
      version: "0.2.0",
      elixir: "~> 1.7.1",
      build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+     start_permanent: Mix.env == :prod]
   end
 
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
   def application do
-    [extra_applications: [:logger, :cowboy],
+    [extra_applications: [:logger],
      mod: {VideoChat, []}]
-  end
-
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do
-    [{:cowboy, "~> 2.6.1"},
-    {:poison, "~> 4.0.1"}]
   end
 end
