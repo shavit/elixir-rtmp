07934696d8d1e47433c50d838596ebbc81313e6c
shavit <shavit@users.noreply.github.com>
2019-02-14 11:06:15 -0500

    Update the example to create stream

--- test/video_chat/rtmp/chunk_test.exs
+++ test/video_chat/rtmp/chunk_test.exs
@@ -5,13 +5,19 @@ defmodule VideoChat.RTMP.ChunkTest do
 
   describe "chunk" do
 
-    test "parse/1 creates a command message" do
+    test "parse/1 parse amf string message" do
       # https://en.wikipedia.org/wiki/Real-Time_Messaging_Protocol#Connect
       msg = <<0x03, #fmt
         0x0, 0x0b, 0x6b, # timestamp
         0x0, 0x0, 0x19, # length
         0x14, # message type id, AMF0 encoded command mesage
         0x0, 0x0, 0x0, 0x0, # message stream id
+        0x02, # amf string marker
+        0x0, 0x0c, # string message size
+        0x63, 0x72, 0x65, 0x61, 0x74, 0x65, 0x53, 0x74, 0x72, 0x65, 0x61, 0x6d, # createStream
+        0x0,  # transaction id
+        0x40, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x5 # null; no more arguments
       >>
       assert {:message, message, _reply} = Chunk.parse msg
       assert %Message{} = message
