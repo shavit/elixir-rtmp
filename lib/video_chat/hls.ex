e18cd48079a5324e8a0f925f8a268a3543db01ce
shavit <shavit@users.noreply.github.com>
2020-03-01 02:11:57 -0500

    Add segments to the playlist

--- lib/video_chat/hls.ex
+++ lib/video_chat/hls.ex
@@ -50,10 +50,52 @@ defmodule VideoChat.HLS do
 
   @tag "#EXTM3U"
 
-  def vod do
+  @doc """
+  new/1 creates a playlist
+  """
+  def new(_opts) do
+    %__MODULE__{
+      cache: false,
+      segments: [],
+      version: 4
+    }
+  end
+
+  @doc """
+  vod/1 creates on-demand playlist
+  """
+  def vod(_struct) do
+    throw("Not implemented")
+  end
+
+  @doc """
+  live/1 creates a live playlist
+  """
+  def live(_struct) do
+    throw("Not implemented")
+  end
+
+  @doc """
+  add_segment/2 appends a segment to the end of the segment list
+
+  It will not check for duplications
+  """
+  def add_segment(%__MODULE__{segments: segments} = struct, %Segment{} = segment) do
+    %{
+      struct
+      | segments: List.insert_at(segments, -1, segment)
+    }
+  end
+
+  @doc """
+  to_string/1 returns the playlist
+  """
+  def to_string(_struct) do
     """
     #EXT-X-VERSION:4
     #EXT-X-ALLOW-CACHE:NO
     """
+
+    throw("Not implemented")
   end
 end
--- test/video_chat/hls_test.exs
+++ test/video_chat/hls_test.exs
@@ -1,21 +1,37 @@
 defmodule VideoChat.HLSTest do
   use ExUnit.Case
+  alias VideoChat.HLS
   alias VideoChat.HLS.Segment
 
-  @valid_attrs %{
+  @valid_segment %{
     duration: 9.0,
     src: "https://example.com/vod/2.ts"
   }
 
+  describe "hls" do
+    test "add_segment/2 appends a segment" do
+      opts = %{cache: true}
+      hls = HLS.new(opts)
+      segment = %Segment{}
+
+      assert %HLS{segments: segments} = segement = HLS.add_segment(hls, segment)
+      assert [segment] == segments
+
+      assert %HLS{segments: segments} = HLS.add_segment(hls, segment)
+      assert [segment] == List.last(segments)
+      assert [segment] == List.first(segments)
+    end
+  end
+
   describe "segment" do
     test "new/2 creates HLS segment struct" do
-      assert %Segment{} = segment = Segment.new(@valid_attrs.src, @valid_attrs.duration)
-      assert segment.duration == @valid_attrs.duration
-      assert segment.src == @valid_attrs.src
+      assert %Segment{} = segment = Segment.new(@valid_segment.src, @valid_segment.duration)
+      assert segment.duration == @valid_segment.duration
+      assert segment.src == @valid_segment.src
     end
 
     test "to_string/1 returns segment tags" do
-      segment = Segment.new(@valid_attrs.src, @valid_attrs.duration)
+      segment = Segment.new(@valid_segment.src, @valid_segment.duration)
 
       expected = """
       #EXTINF:9.0
