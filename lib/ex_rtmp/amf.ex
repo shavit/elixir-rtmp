6651544061e9a2e20405ce8723ee2bdc671436ed
shavit <shavit@users.noreply.github.com>
2020-08-20 13:11:38 -0400

    encode values

--- lib/ex_rtmp/amf.ex
+++ lib/ex_rtmp/amf.ex
@@ -2,30 +2,34 @@ defmodule ExRTMP.AMF do
   @moduledoc """
   `ExRTMP.AMF` reader and writer
 
-  
+
   """
 
   def decode(msg) do
     msg
   end
 
-  @doc"""
+  @doc """
   encode_key/1 encodes AMF object key
   """
   def encode_key(key) when is_binary(key) do
     <<byte_size(key)::unsigned-integer>> <> key
   end
 
-  @doc"""
+  @doc """
   encode_number/1 encodes AMF number value
 
   The format is IEEE 64-bit double-precision floating point
   """
-  def encode_number(value) when is_integer(value) do
-    <<0x0, value::float-64>>
+  def encode_number(value) do
+    if is_number(value) do
+      <<0x0, value::float-64>>
+    else
+      {:error, "invalid input"}
+    end
   end
 
-  @doc"""
+  @doc """
   encode_boolean/1 encodes AMF boolean value
   """
   def encode_boolean(value) do
@@ -38,22 +42,35 @@ defmodule ExRTMP.AMF do
     end
   end
 
-  @doc"""
+  @doc """
   encode_string/1 encodes AMF string value
   """
   def encode_string(value) do
+    if is_binary(value) do
+      <<0x02, value::binary>>
+    else
+      {:error, "invalid input"}
+    end
   end
 
-  @doc"""
+  @doc """
   encode_object/1 encodes AMF object value
   """
   def encode_object(value) do
+    if is_map(value) do
+    else
+      {:error, "invalid input"}
+    end
   end
 
-  @doc"""
+  @doc """
   encode_null/1 encodes AMF null value
   """
   def encode_null(value) do
+    if is_nil(value) do
+      <<0x05>>
+    else
+      {:error, "invalid input"}
+    end
   end
-
 end
--- test/ex_rtmp/amf_test.exs
+++ test/ex_rtmp/amf_test.exs
@@ -7,21 +7,59 @@ defmodule ExRTMP.AMFTest do
   end
 
   describe "amf" do
-
     test "encode_key/1 encodes object key" do
       [
-      	{&AMF.encode_key/1, "some key", <<0x8, 115, 111, 109, 101, 32, 107, 101, 121>>}
-      ] |> Enum.each(&assert_test_case/1)
+        {&AMF.encode_key/1, "some key", <<0x8, 115, 111, 109, 101, 32, 107, 101, 121>>}
+      ]
+      |> Enum.each(&assert_test_case/1)
+    end
+
+    test "encode_number/1 encodes boolean value" do
+      [
+        {&AMF.encode_number/1, 0, <<0x00, 0x0, 0, 0, 0, 0, 0, 0, 0>>},
+        {&AMF.encode_number/1, 12, <<0x00, 64, 40, 0, 0, 0, 0, 0, 0>>},
+        {&AMF.encode_number/1, 0.11, <<0x0, 63, 188, 40, 245, 194, 143, 92, 41>>},
+        {&AMF.encode_number/1, "something", {:error, "invalid input"}}
+      ]
+      |> Enum.each(&assert_test_case/1)
     end
 
     test "encode_boolean/1 encodes boolean value" do
       [
-    	{&AMF.encode_boolean/1, true, <<0x01, 0x01>>},
-    	{&AMF.encode_boolean/1, false, <<0x01, 0x00>>},
-    	{&AMF.encode_boolean/1, 1, <<0x01, 0x01>>},
-    	{&AMF.encode_boolean/1, 0, <<0x01, 0x00>>},
-    	{&AMF.encode_boolean/1, "something", {:error, "invalid input"}},
-      ] |> Enum.each(&assert_test_case/1)      
+        {&AMF.encode_boolean/1, true, <<0x01, 0x01>>},
+        {&AMF.encode_boolean/1, false, <<0x01, 0x00>>},
+        {&AMF.encode_boolean/1, 1, <<0x01, 0x01>>},
+        {&AMF.encode_boolean/1, 0, <<0x01, 0x00>>},
+        {&AMF.encode_boolean/1, "something", {:error, "invalid input"}}
+      ]
+      |> Enum.each(&assert_test_case/1)
+    end
+
+    test "encode_string/1 encodes string value" do
+      [
+        {&AMF.encode_string/1, "some value", <<0x02, 115, 111, 109, 101, 32, 118, 97, 108, 117, 101>>},
+        {&AMF.encode_string/1, 1234, {:error, "invalid input"}}
+      ]
+      |> Enum.each(&assert_test_case/1)
+    end
+
+      test "encode_objecet/1 encodes object value" do
+      [
+
+        {&AMF.encode_string/1, 1234, {:error, "invalid input"}},
+	{&AMF.encode_string/1, [1,2,3,4], {:error, "invalid input"}},
+	{&AMF.encode_string/1, {1,2,3,4}, {:error, "invalid input"}},
+      ]
+      |> Enum.each(&assert_test_case/1)
+    end
+
+      test "encode_null/1 encodes null value" do
+      [
+        {&AMF.encode_null/1, nil, <<0x05>>},
+        {&AMF.encode_null/1, 1234, {:error, "invalid input"}},
+	        {&AMF.encode_null/1, "null", {:error, "invalid input"}}
+      ]
+      |> Enum.each(&assert_test_case/1)
     end
   end
 end
