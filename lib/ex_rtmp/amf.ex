698ae1dc8b913e75daaea0d675db11873279f638
shavit <shavit@users.noreply.github.com>
2020-08-20 13:02:21 -0400

    test boolean value

--- lib/ex_rtmp/amf.ex
+++ lib/ex_rtmp/amf.ex
@@ -9,7 +9,51 @@ defmodule ExRTMP.AMF do
     msg
   end
 
+  @doc"""
+  encode_key/1 encodes AMF object key
+  """
   def encode_key(key) when is_binary(key) do
     <<byte_size(key)::unsigned-integer>> <> key
   end
+
+  @doc"""
+  encode_number/1 encodes AMF number value
+
+  The format is IEEE 64-bit double-precision floating point
+  """
+  def encode_number(value) when is_integer(value) do
+    <<0x0, value::float-64>>
+  end
+
+  @doc"""
+  encode_boolean/1 encodes AMF boolean value
+  """
+  def encode_boolean(value) do
+    case value do
+      false -> <<0x01, 0x00>>
+      true -> <<0x01, 0x01>>
+      0 -> <<0x01, 0x0>>
+      1 -> <<0x01, 0x01>>
+      _ -> {:error, "invalid input"}
+    end
+  end
+
+  @doc"""
+  encode_string/1 encodes AMF string value
+  """
+  def encode_string(value) do
+  end
+
+  @doc"""
+  encode_object/1 encodes AMF object value
+  """
+  def encode_object(value) do
+  end
+
+  @doc"""
+  encode_null/1 encodes AMF null value
+  """
+  def encode_null(value) do
+  end
+
 end
--- test/ex_rtmp/amf_test.exs
+++ test/ex_rtmp/amf_test.exs
@@ -2,16 +2,26 @@ defmodule ExRTMP.AMFTest do
   use ExUnit.Case
   alias ExRTMP.AMF
 
-  defp assert_test_case({input, expect}) do
-    assert AMF.encode_key(input) == expect
+  defp assert_test_case({fun, input, expect}) do
+    assert fun.(input) == expect
   end
 
   describe "amf" do
 
     test "encode_key/1 encodes object key" do
       [
-	{"some key", <<0x8, 115, 111, 109, 101, 32, 107, 101, 121>>}
+      	{&AMF.encode_key/1, "some key", <<0x8, 115, 111, 109, 101, 32, 107, 101, 121>>}
       ] |> Enum.each(&assert_test_case/1)
+    end
+
+    test "encode_boolean/1 encodes boolean value" do
+      [
+    	{&AMF.encode_boolean/1, true, <<0x01, 0x01>>},
+    	{&AMF.encode_boolean/1, false, <<0x01, 0x00>>},
+    	{&AMF.encode_boolean/1, 1, <<0x01, 0x01>>},
+    	{&AMF.encode_boolean/1, 0, <<0x01, 0x00>>},
+    	{&AMF.encode_boolean/1, "something", {:error, "invalid input"}},
+      ] |> Enum.each(&assert_test_case/1)      
     end
   end
 end
