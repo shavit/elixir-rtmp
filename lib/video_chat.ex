da8cdda9da57b65859719227d43dffc933612016
shavit <shavit@users.noreply.github.com>
2020-08-23 14:02:08 -0400

    accept port from environment variable

--- lib/video_chat.ex
+++ lib/video_chat.ex
@@ -8,7 +8,7 @@ defmodule VideoChat do
 
   def start(_type, _args) do
     children = [
-      ExRTMPServer
+      {ExRTMPServer, [port: "RTMP_PORT" |> System.get_env("1935") |> String.to_integer()]}
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one)
