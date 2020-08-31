3638f30be5b7f91b5c39facb45b4dc5a74ba9f58
shavit <shavit@users.noreply.github.com>
2020-08-31 08:28:16 -0400

    add encoder

---
+++ lib/ex_rtmp/encoder.ex
@@ -0,0 +1,26 @@
+defmodule ExRTMP.Encoder do
+  @moduledoc """
+  Documentation for `ExRTMP.Encoder`
+  """
+  use GenServer
+  require Logger
+
+  def start_link(opts) do
+    name = Keyword.get(opts, :name, __MODULE__)
+    GenServer.start_link(__MODULE__, opts, name: name)
+  end
+
+  def init(opts) do
+    state = %{
+      server: Keyword.get(opts, :server)
+    }
+
+    {:ok, state}
+  end
+
+  def handle_call({:encode, msg}, _ref, state) do
+    Logger.info("[Encoder] encoder message: #{inspect(msg)}")
+
+    {:ok, state, state}
+  end
+end
---
+++ test/ex_rtmp/encoder_test.exs
@@ -0,0 +1,7 @@
+defmodule ExRTMP.EncoderTest do
+  use ExUnit.Case
+  alias ExRTMP.Encoder
+
+  describe "encoder" do
+  end
+end
