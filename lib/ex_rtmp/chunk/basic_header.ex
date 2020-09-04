8ac2ee1b2c6b09a649a8f8b6eaebe3f0ef94f81b
shavit <shavit@users.noreply.github.com>
2020-09-04 10:00:11 -0400

    test basic header

--- lib/ex_rtmp/chunk/basic_header.ex
+++ lib/ex_rtmp/chunk/basic_header.ex
@@ -5,9 +5,39 @@ defmodule ExRTMP.Chunk.BasicHeader do
 
   defstruct [:stream_id, :type]
 
+  @doc """
+  new/2 creates a basic header
 
-  def new(csid, :one) when csid > 2 and csid <= 65_599 do
+    * Type 0 - 1 byte
+    * Type 1 - 2 bytes
+    * Type 2 - 3 bytes
+
+  ## Params
+    * fmt - The chunk type, that will determine the message header
+    * csid - The size will determine basic header format
+  """
+  def new(csid, :one) when csid > 2 and csid <= 63 do
     <<0::2, csid::6>>
   end
+  def new(csid, :two) when csid > 2 and csid <= 63 do
+    <<1::2, csid::6>>
+  end
+  def new(csid, :three) when csid > 2 and csid <= 63 do
+    <<2::2, csid::6>>
+  end
+
   def new(_csid, :one), do: {:error, "id out of range"}
+
+  def new(csid, :two) when csid >= 64 and csid <= 319 do
+    <<1::2, 0::6, csid - 64::8>>
+  end
+
+  def new(_csid, :two), do: {:error, "id out of range"}
+
+  def new(csid, :three) when csid >= 64 and csid <= 65_599 do
+    csid = csid-64
+    <<1::2, 1::6, csid::16>>
+  end
+
+  def new(_csid, :three), do: {:error, "id out of range"}
 end
--- test/ex_rtmp/chunk/basic_header_test.exs
+++ test/ex_rtmp/chunk/basic_header_test.exs
@@ -8,16 +8,43 @@ defmodule ExRTMP.Chunk.BasicHeaderTest do
 
   describe "chunk basic header" do
     test "new/2 creates basic header type 1" do
-      #BasicHeader.new 3, :one
       [
-	{BasicHeader, :new, [1, :one], {:error, "id out of range"}},
-	{BasicHeader, :new, [2, :one], {:error, "id out of range"}},
-	{BasicHeader, :new, [3, :one], <<0::2, 3::6>>},
-	{BasicHeader, :new, [4, :one], <<0::2, 4::6>>},
-	{BasicHeader, :new, [65_599, :one], <<0::2, 65_599::6>>},
-	{BasicHeader, :new, [65_600, :one], {:error, "id out of range"}},
-      ] |> Enum.each(&assert_expected/1)
+        {BasicHeader, :new, [1, :one], {:error, "id out of range"}},
+        {BasicHeader, :new, [2, :one], {:error, "id out of range"}},
+        {BasicHeader, :new, [3, :one], <<0::2, 3::6>>},
+        {BasicHeader, :new, [4, :one], <<0::2, 4::6>>},
+        {BasicHeader, :new, [63, :one], <<0::2, 63::6>>},
+        {BasicHeader, :new, [64, :one], {:error, "id out of range"}}
+      ]
+      |> Enum.each(&assert_expected/1)
+    end
+
+    test "new/2 creates basic header type 2" do
+      [
+        # {BasicHeader, :new, [1, :two], {:error, "id out of range"}},
+        # {BasicHeader, :new, [2, :two], {:error, "id out of range"}},
+        # {BasicHeader, :new, [64, :two], <<0::2, 0::6, 0::8>>},
+	{BasicHeader, :new, [64, :two], <<64, 0>>},
+	{BasicHeader, :new, [65, :two], <<64, 1>>},
+        # {BasicHeader, :new, [65, :two], <<0::2, 0::6, 1::8>>},
+#        {BasicHeader, :new, [319, :two], <<0::2, 0::6, 319::8>>},
+#        {BasicHeader, :new, [320, :two], {:error, "id out of range"}}
+      ]
+      |> Enum.each(&assert_expected/1)
+    end
+
+    test "new/2 creates basic header type 3" do
+      [
+#        {BasicHeader, :new, [1, :three], {:error, "id out of range"}},
+#        {BasicHeader, :new, [2, :three], {:error, "id out of range"}},
+#        {BasicHeader, :new, [63, :three], {:error, "id out of range"}},
+#        {BasicHeader, :new, [64, :three], <<1::2, 1::6, 0::6>>},
+        # {BasicHeader, :new, [100, :three], <<1::2, 1::6, 36::16>>},
+        # {BasicHeader, :new, [200, :three], <<1::2, 1::6, 136::16>>},
+#        {BasicHeader, :new, [65_599, :three], <<1::2, 1::6, 255::16>>},
+        {BasicHeader, :new, [65_600, :three], {:error, "id out of range"}}
+      ]
+      |> Enum.each(&assert_expected/1)
     end
   end
 end
-
