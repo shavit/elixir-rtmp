27d8be51e16bc63ad6c3c3f0cc11bb768972a66c
shavit <shavit@users.noreply.github.com>
2020-09-05 16:40:15 -0400

    add message header

---
+++ lib/ex_rtmp/chunk/message_header.ex
@@ -0,0 +1,33 @@
+defmodule ExRTMP.Chunk.MessageHeader do
+  @moduledoc """
+  `ExRTMP.Chunk.MessageHeader` encodes the message header
+  """
+
+  @doc """
+  decode/1 decodes message header
+  """
+
+  def decode(<<_fmt::2, 1::6, _csid::16, msg::binary>>) do
+    msg
+  end
+
+  def decode(<<_fmt::2, 0::6, _csid::8, msg::binary>>) do
+    msg
+  end
+
+  def decode(<<0::2, _csid::6, msg::binary>>) do
+    case msg do
+      <<tm::24, l::16, mt::8, msid::32, b::binary>> ->
+        %{
+          timestamp: tm,
+          length: l,
+          message_type_id: mt,
+          message_stream_id: msid,
+	  body: b
+        }
+
+      _ ->
+        {:error, "could not decode message header"}
+    end
+  end
+end
---
+++ test/ex_rtmp/chunk/message_header_test.exs
@@ -0,0 +1,15 @@
+defmodule ExRTMP.Chunk.MessageHeaderTest do
+  use ExUnit.Case
+  alias ExRTMP.Chunk.MessageHeader
+
+  describe "message header" do
+    test "decode/1 decodes message header" do
+      tests = [
+      <<2, 0, 0, 0, 0, 0, 6, 4, 0, 0, 0, 0, 0, 6, 17, 249, 187, 163>>
+      ] |> Enum.each(fn x ->
+	IO.inspect MessageHeader.decode(x)
+      end)      
+    end
+
+  end
+end
