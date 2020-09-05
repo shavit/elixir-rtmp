d549f0709a1d0eba0d41d178483f11cb98fa484e
shavit <shavit@users.noreply.github.com>
2020-09-05 16:22:43 -0400

    basic header 3

--- lib/ex_rtmp/chunk/basic_header.ex
+++ lib/ex_rtmp/chunk/basic_header.ex
@@ -19,20 +19,13 @@ defmodule ExRTMP.Chunk.BasicHeader do
   def new(csid, :one) when csid > 2 and csid <= 63 do
     <<0::2, csid::6>>
   end
-  def new(csid, :two) when csid > 2 and csid <= 63 do
-    <<1::2, csid::6>>
-  end
-  def new(csid, :three) when csid > 2 and csid <= 63 do
-    <<2::2, csid::6>>
-  end
 
   def new(csid, :two) when csid >= 64 and csid <= 319 do
     <<1::2, 0::6, csid - 64::8>>
   end
 
   def new(csid, :three) when csid >= 64 and csid <= 65_599 do
-    csid = csid-64
-    <<1::2, 1::6, csid::16>>
+    <<1::2, 1::6, csid - 64::16-little>>
   end
 
   def new(_csid, :one), do: {:error, "id out of range"}
--- test/ex_rtmp/chunk/basic_header_test.exs
+++ test/ex_rtmp/chunk/basic_header_test.exs
@@ -11,13 +11,7 @@ defmodule ExRTMP.Chunk.BasicHeaderTest do
       [
         {BasicHeader, :new, [3, :one], <<0::2, 3::6>>},
         {BasicHeader, :new, [4, :one], <<0::2, 4::6>>},
-        {BasicHeader, :new, [63, :one], <<0::2, 63::6>>},
-	{BasicHeader, :new, [3, :two], <<1::2, 3::6>>},
-	{BasicHeader, :new, [10, :two], <<1::2, 10::6>>},
-	{BasicHeader, :new, [63, :two], <<1::2, 63::6>>},
-	{BasicHeader, :new, [3, :three], <<2::2, 3::6>>},
-	{BasicHeader, :new, [10, :three], <<2::2, 10::6>>},
-	{BasicHeader, :new, [63, :three], <<2::2, 63::6>>},
+        {BasicHeader, :new, [63, :one], <<0::2, 63::6>>}
       ]
       |> Enum.each(&assert_expected/1)
     end
@@ -25,27 +19,20 @@ defmodule ExRTMP.Chunk.BasicHeaderTest do
     test "new/2 creates basic header type 2" do
       [
         {BasicHeader, :new, [64, :two], <<1::2, 0::6, 0::8>>},
-	{BasicHeader, :new, [65, :two], <<1::2, 0::6, 1::8>>},
-	{BasicHeader, :new, [80, :two], <<1::2, 0::6, 16::8>>},
-	{BasicHeader, :new, [319, :two], <<1::2, 0::6, 255::8>>},
-#	{BasicHeader, :new, [64, :three], <<1::2, 1::6, 0::8>>},
-        # {BasicHeader, :new, [65, :two], <<0::2, 0::6, 1::8>>},
-#        {BasicHeader, :new, [319, :two], <<0::2, 0::6, 319::8>>},
-#        {BasicHeader, :new, [320, :two], {:error, "id out of range"}}
+        {BasicHeader, :new, [65, :two], <<1::2, 0::6, 1::8>>},
+        {BasicHeader, :new, [80, :two], <<1::2, 0::6, 16::8>>},
+        {BasicHeader, :new, [319, :two], <<1::2, 0::6, 255::8>>}
       ]
       |> Enum.each(&assert_expected/1)
     end
 
     test "new/2 creates basic header type 3" do
       [
-#        {BasicHeader, :new, [1, :three], {:error, "id out of range"}},
-#        {BasicHeader, :new, [2, :three], {:error, "id out of range"}},
-#        {BasicHeader, :new, [63, :three], {:error, "id out of range"}},
-#        {BasicHeader, :new, [64, :three], <<1::2, 1::6, 0::6>>},
-        # {BasicHeader, :new, [100, :three], <<1::2, 1::6, 36::16>>},
-        # {BasicHeader, :new, [200, :three], <<1::2, 1::6, 136::16>>},
-#        {BasicHeader, :new, [65_599, :three], <<1::2, 1::6, 255::16>>},
-        {BasicHeader, :new, [65_600, :three], {:error, "id out of range"}}
+        {BasicHeader, :new, [64, :three], <<1::2, 1::6, 0::16>>},
+        {BasicHeader, :new, [100, :three], <<1::2, 1::6, 36, 0>>},
+        {BasicHeader, :new, [200, :three], <<1::2, 1::6, 136, 0>>},
+        {BasicHeader, :new, [255, :three], <<1::2, 1::6, 191, 0>>},
+        {BasicHeader, :new, [65_599, :three], <<1::2, 1::6, 255, 255>>}
       ]
       |> Enum.each(&assert_expected/1)
     end
@@ -54,16 +41,15 @@ defmodule ExRTMP.Chunk.BasicHeaderTest do
       [
         {BasicHeader, :new, [0, :one], {:error, "id out of range"}},
         {BasicHeader, :new, [1, :one], {:error, "id out of range"}},
-	{BasicHeader, :new, [2, :one], {:error, "id out of range"}},
+        {BasicHeader, :new, [2, :one], {:error, "id out of range"}},
         {BasicHeader, :new, [0, :two], {:error, "id out of range"}},
         {BasicHeader, :new, [1, :two], {:error, "id out of range"}},
-	{BasicHeader, :new, [2, :two], {:error, "id out of range"}},
+        {BasicHeader, :new, [2, :two], {:error, "id out of range"}},
         {BasicHeader, :new, [0, :three], {:error, "id out of range"}},
         {BasicHeader, :new, [1, :three], {:error, "id out of range"}},
-	{BasicHeader, :new, [2, :three], {:error, "id out of range"}},
+        {BasicHeader, :new, [2, :three], {:error, "id out of range"}}
       ]
       |> Enum.each(&assert_expected/1)
     end
-
   end
 end
