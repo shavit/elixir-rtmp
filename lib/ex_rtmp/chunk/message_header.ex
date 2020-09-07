121d9516194de567ff12808cebcbdba92a4d4a5e
shavit <shavit@users.noreply.github.com>
2020-09-07 07:00:00 -0400

    decode control message header

--- lib/ex_rtmp/chunk/message_header.ex
+++ lib/ex_rtmp/chunk/message_header.ex
@@ -8,26 +8,26 @@ defmodule ExRTMP.Chunk.MessageHeader do
   """
 
   def decode(<<_fmt::2, 1::6, _csid::16, msg::binary>>) do
-    msg
+    decode_message_header(msg)
   end
 
   def decode(<<_fmt::2, 0::6, _csid::8, msg::binary>>) do
-    msg
+    decode_message_header(msg)
   end
 
   def decode(<<0::2, _csid::6, msg::binary>>) do
-    case msg do
-      <<tm::24, l::16, mt::8, msid::32, b::binary>> ->
-        %{
-          timestamp: tm,
-          length: l,
-          message_type_id: mt,
-          message_stream_id: msid,
-	  body: b
-        }
-
-      _ ->
-        {:error, "could not decode message header"}
-    end
+    decode_message_header(msg)
   end
+
+  defp decode_message_header(<<tm::24, l::16, mt::8, msid::32, b::binary>>) do
+    %{
+      timestamp: tm,
+      length: l,
+      message_type_id: mt,
+      message_stream_id: msid,
+      body: b
+    }
+  end
+
+  defp decode_message_header(_msg), do: {:error, "could not decode message header"}
 end
