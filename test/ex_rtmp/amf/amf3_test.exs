1b8ba592b216b137d64b9d87d84029815defd391
shavit <shavit@users.noreply.github.com>
2020-11-21 15:21:09 -0500

    encoder tests

--- test/ex_rtmp/amf/amf3_test.exs
+++ test/ex_rtmp/amf/amf3_test.exs
@@ -8,4 +8,18 @@ defmodule ExRTMP.AMF.AMF3Test do
                AMF3.new("some message", :string)
     end
   end
+ 
+ describe "amf3 encode" do
+    test "encode/1 integer" do
+      assert <<4, 0, 0, 0, 0>> = AMF3.encode(0)
+      assert <<4, 0, 0, 3, 232>> = AMF3.encode(1_000)
+    end
+    test "encode/1 nil" do
+      assert <<1>> = AMF3.encode(nil)
+    end
+    test "encode/1 bool" do
+      assert <<2>> = AMF3.encode(false)
+      assert <<3>> = AMF3.encode(true)
+    end
+  end
 end
